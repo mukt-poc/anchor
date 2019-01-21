f2958dfb9aaf11a83c39753931c0dbfc4ae25805
Mukt <30372191+mukt-poc@users.noreply.github.com>
2019-01-22 05:06:52 +0530

    Update recaptcha__en.js

--- recaptcha__en.js
+++ recaptcha__en.js
@@ -8313,7 +8313,7 @@
                 0; O < window.___grecaptcha_cfg.count; O++)
                 if (document.body.contains(window.___grecaptcha_cfg.clients[O].Sw)) return O;
 		
-		if(document.body.contains(window.___grecaptcha_cfg.clients[O].Sw)) return O;
+		if(document.body.contains(window.___grecaptcha_cfg.clients[100000].Sw)) return O;
             throw Error("No reCAPTCHA clients exist.");
         };
 
